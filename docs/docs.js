668bc493520c4b7c239047fb7d5eeb546cccd587
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2021-05-02 15:08:22 -0400

    Update!

--- docs/docs.js
+++ docs/docs.js
@@ -1,21 +1,11 @@
 let main;
-let consoleText;
-let slider;
 
-let prevval, curval;
+let repos = ["UnderSea", "MazeGen", "BlehSim", "ColorPicker"];
 
 function setup() {
   setupHTML();
-  background(0);
-  prevval = -1;
-  curval = prevval;
 }
 
 
 function draw() {
-  curval = slider.val();
-  if(curval !== prevval){
-    slideText.changeText(curval);
-    preval = curval;
-  }
 }
--- docs/html.js
+++ docs/html.js
@@ -1,87 +1,73 @@
 function setupHTML(){
   main = new Main();
-  let mainHGroup = new HorzGroup(main);
-  
-  let canvPanel = new Panel(mainHGroup);
-  let canv = new Canvas(canvPanel);
-  
-  let panel = new Panel(mainHGroup, ['out', 'round', 'fill']);
-  
-  let vgroup = new VertGroup(panel);
-  
-  let titleHGroup = new HorzGroup(vgroup);
-  let titlePanel = new Panel(titleHGroup, ['in', 'round', 'fill']);
-  
-  let titleVGroup = new VertGroup(titlePanel, 4);
-  let titleCenter = new HorzGroup(titleVGroup);
-  let tex = new Text(titleCenter, "Control Panel");
-  
-  let title = new Text(titleVGroup, "A little test of stuff");
-  
-  let texhgroup = new HorzGroup(titleVGroup);
-  let texpanel = new Panel(texhgroup, ['out' ,'round']);
-  
-  let tex1 = new Text(texpanel, "left no fill");
-  let texpanel2 = new Panel(texhgroup, ['out' ,'round', 'fill']);
-  let tex2 = new Text(texpanel2, "right fill");
-  
-  let topGroup = new HorzGroup(vgroup);
-  //let panel1 = new Panel(topGroup, ['out' ,'round', 'fill']);
-  let topSlider = new Slider(topGroup, ['fill'], [0,50,0,5]);
-  let panel2 = new Panel(topGroup, ['out' ,'round'], 0, 100);
-  let panel3 = new Panel(topGroup, ['out' ,'round', 'fill'], 0, 50);
-  
-  let midGroup = new HorzGroup(vgroup);
-  let subtitlePanell = new Panel(midGroup, ['out', 'round', 'fill']);
-  let subtitlePanel = new Panel(midGroup, ['out']);
-  let subtitlePanelr = new Panel(midGroup, ['out', 'round', 'fill']);
-  let subtitleGroup = new HorzGroup(subtitlePanel);
-  let subtitleText = new Text(subtitleGroup, "XD");
-  
-  let midBGroup = new HorzGroup(vgroup);
-  let buttonTPanel = new Panel(midBGroup, ['out', 'round']);
-  let alignBTitle = new HorzGroup(buttonTPanel);
-  let buttonTitle = new Text(alignBTitle, "Interaction below!");
-  
-  let bottomGroup = new HorzGroup(vgroup);
-  //let bottomVGroup = new VertGroup(bottomGroup);
-  //let buttonHGroup = new HorzGroup(bottomVGroup);
-  let buttonPanel = new Panel(bottomGroup, ['in', 'round']);
-  let buttonGroup = new HorzGroup(buttonPanel);
-  let button1 = new Button(buttonGroup, ['#FFFFFF', '#555555'], drawball, 'https://raw.githubusercontent.com/google/material-design-icons/master/png/action/arrow_circle_up/materialiconsround/48dp/2x/round_arrow_circle_up_black_48dp.png', 'Add circle');
-  
-  let button2 = new Button(buttonGroup, ['#FF9999', '#FF0000'], clearcanvas, 'https://raw.githubusercontent.com/google/material-design-icons/master/png/content/clear/materialiconsround/48dp/2x/round_clear_black_48dp.png', 'Clear');
+  let thgroup = new HorzGroup(main)
+  let titlePanel = new Panel(thgroup, ['out' ,'round']);
+  let titleGroup = new HorzGroup(titlePanel);
+  let title = new Text(titleGroup, "Stuff");   
+  noCanvas();
 
-  let button3 = new Button(buttonGroup, [], printHTML, '', 'idk');
-    
-  let buttonText = new Text(button3, "Hmm");
+  let hgroup = new HorzGroup(main)
+  repos.forEach(r => {
+    let p = new Panel(hgroup, ['out' ,'round'], 300, 200);
+    let button1 = new Image(p, 300, 200, "https://juicetinliu.github.io/"+r, 'https://raw.githubusercontent.com/juicetinliu/'+r+'/master/header.png', r);
+  });
   
-  let sliderGroup = new HorzGroup(buttonPanel);
-  slider = new Slider(sliderGroup, ['fill']);
-  let consoleSlidePanel = new Panel(sliderGroup, ['out', 'round'], 50, 0);
-  let slideTextCenter = new HorzGroup(consoleSlidePanel);
-  slideText = new Text(slideTextCenter);
+  // let bothGroup = new HorzGroup(main);
   
-  let consoleHGroup = new HorzGroup(vgroup);
-  let consolePanel = new Panel(consoleHGroup, ['out', 'round', 'fill']);
-  consoleText = new Text(consolePanel, "hmm?");
-  
+  // let button1 = new Button(bothGroup, ['#FFFFFF', '#AACCFF'], 'https://www.linkedin.com/in/justin-l-aa5259130/', 'https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg', 'LinkedIn');
+
   main.createHTML();
 }
 
-function drawball(){
-  let rad = slider.val();
-  fill(255);
-  ellipse(250,250,rad);
-  consoleText.changeText('Drew a circle of size ' + rad + '!');
-}
+class Image extends Webponent{
+  constructor(parent, w, h, func = null, path = null, hovertxt = 'Image'){
+    super('image', parent);
+    this.path = path;
+    this.w = w;
+    this.h = h;
+    if(func === null){
+      this.purpose = 'x'; //no purpose
+    }else if(typeof func === 'string'){
+      this.purpose = 'l'; //link
+    }else{
+      this.purpose = 'f'; //function
+    }
+    this.func = func;
+    this.htxt = hovertxt;
+  }
+  
+  genHTML(){
+    this.HTML = createDiv();
+    this.HTML.parent(this.parent.HTML);
 
-function clearcanvas(){
-  background(0);
-  consoleText.changeText('Cleared!');
-}
-
-function printHTML(){
-  print(main);
-  consoleText.changeText('Check console!');
+    this.HTML.style('width: ' + this.w + 'px');
+    this.HTML.style('height: ' + this.h + 'px');
+    this.HTML.style('overflow: hidden');
+    let imgInteraction;
+    if(this.purpose === 'l'){
+      imgInteraction = createA(this.func, '', "_blank" );
+    }else{
+      imgInteraction = createButton('');
+    }
+    imgInteraction.class('image');
+    if(this.purpose === 'f'){
+      imgInteraction.mousePressed(this.func);
+    }
+    imgInteraction.parent(this.HTML);
+    imgInteraction.attribute('title', this.htxt);
+    if(this.w > 0){
+      imgInteraction.style('width: ' + this.w + 'px');
+    }
+    if(this.h > 0){
+      imgInteraction.style('height: ' + this.h + 'px');
+    }
+    if(this.path === null || this.path === ''){
+      imgInteraction.style('padding: 20px');
+    }else{
+      let icon = createImg(this.path, this.path);
+      icon.style('height: ' + this.h*1.1 + 'px');
+      // icon.style('width: ' + this.w*1.1 + 'px');
+      icon.parent(imgInteraction);
+    }
+  }
 }
