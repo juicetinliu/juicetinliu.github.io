dce8ad943744f2e3d8200a4c5c21992165a870d6
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2021-05-04 01:54:27 -0400

    Update docs.js

--- docs/docs.js
+++ docs/docs.js
@@ -1,6 +1,6 @@
 let main;
 
-let repos = [["UnderSea", "An underwater adventure"], ["MazeGen", "Maze generation and pathfinding"], ["BlehSim", "A little virus simulation"], ["ColorPicker", "RGB/HSV to color name converter"], ["Webponents", "A component based based based based based based html wrapper for P5.js"]];
+let repos = [["UnderSea", "An underwater adventure"], ["MazeGen", "Maze generation and pathfinding"], ["BlehSim", "A little virus simulation"], ["ColorPicker", "RGB/HSV to color name converter"], ["Webponents", "A component based html wrapper for P5.js"]];
 
 function setup() {
   setupHTML();
