028f854ab731ff5bbf550a901538c981f99f58cb
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2021-07-19 18:12:51 +0800

    Button and Card Analytics

--- docs/html.js
+++ docs/html.js
@@ -1,9 +1,9 @@
 function setupHTML(){
   main = new Main();
   let thgroup = new HorzGroup(main)
-  let titlePanel = new Panel(thgroup, ['out' ,'round']);
-  let titleGroup = new HorzGroup(titlePanel);
-  let title = new Text(titleGroup, "My Stuff");   
+  // let titlePanel = new Panel(thgroup, ['out' ,'round']);
+  // let titleGroup = new HorzGroup(titlePanel);
+  // let title = new Text(titleGroup, "My Stuff");   
   noCanvas();
 
   let hgroup = new HorzGroup(main)
@@ -12,11 +12,62 @@ function setupHTML(){
     let card = new Card(p, 300, 200, "https://juicetinliu.github.io/"+r[0], 'https://raw.githubusercontent.com/juicetinliu/'+r[0]+'/master/header.png', [r[0],r[1]]);
   });
   
-  // let bothGroup = new HorzGroup(main);
+  let bothGroup = new HorzGroup(main);
   
-  // let button1 = new Button(bothGroup, ['#FFFFFF', '#AACCFF'], 'https://www.linkedin.com/in/justin-l-aa5259130/', 'https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg', 'LinkedIn');
+  let button1 = new trackedButton(bothGroup, ['#FFFFFF', '#AACCFF'], 'https://www.linkedin.com/in/justin-l-aa5259130/', 'linkedin.png', 'LinkedIn');
+  let button2 = new trackedButton(bothGroup, ['#FFFFFF', '#AAAAAA'], 'https://github.com/juicetinliu', 'github.png', 'Github');
 
   main.createHTML();
+}
+
+class trackedButton extends Webponent{
+  constructor(parent, colors, func = null, iconpath = null, hovertxt = 'Button'){
+    super('button', parent);
+    this.iconpath = iconpath;
+    if(colors.length){
+      this.clr = colors[0];
+      this.hclr = colors[1];
+    }else{
+      this.clr = '#e9e9e9';
+      this.hclr = '#aaaaaa';
+    }
+    if(func === null){
+      this.purpose = 'x'; //no purpose
+    }else if(typeof func === 'string'){
+      this.purpose = 'l'; //link
+    }else{
+      this.purpose = 'f'; //function
+    }
+    this.func = func;
+    this.htxt = hovertxt;
+  }
+  
+  genHTML(){
+    if(this.purpose === 'l'){
+      this.HTML = createA(this.func, '', "_blank" );
+    }else{
+      this.HTML = createButton('');
+    }
+    this.HTML.class('butt');
+    if(this.purpose === 'f'){
+      this.HTML.mousePressed(this.func);
+    }
+
+    this.HTML.attribute('onclick', "gtag('event', 'click', {'event_category': 'button', 'event_label': '" + this.htxt + "'});");
+
+    this.HTML.parent(this.parent.HTML);
+    this.HTML.attribute('title', this.htxt);
+    this.HTML.style('background-color: ' + this.clr);
+    this.HTML.attribute('onmouseover', 'this.style.backgroundColor = \'' + this.hclr + '\'');
+    this.HTML.attribute('onmouseout', 'this.style.backgroundColor = \'' + this.clr + '\'');
+    if(this.iconpath === null || this.iconpath === ''){
+      this.HTML.style('padding: 20px');
+    }else{
+      let icon = createImg(this.iconpath, this.iconpath);
+      icon.style('width: 30px');
+      icon.parent(this.HTML);
+    }
+  }
 }
 
 class Card extends Webponent{
@@ -46,7 +97,10 @@ class Card extends Webponent{
     this.HTML.style('border-radius: 5px;')
     this.HTML.style('overflow: hidden');
     this.HTML.style('position: relative');
+    
 
+    this.HTML.attribute('onclick', "gtag('event', 'click', {'event_category': 'button', 'event_label': '" + this.htxt[0] + "'});");
+    
     let imgInteraction;
     if(this.purpose === 'l'){
       imgInteraction = createA(this.func, '', "_blank" );
