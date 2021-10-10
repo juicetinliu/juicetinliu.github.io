9512981bcf234988ab6e282c6084c3def05dc68e
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2021-10-10 14:47:30 -0700

    New repo class; cleanup and added more projects

--- docs/docs.js
+++ docs/docs.js
@@ -1,6 +1,15 @@
 let main;
 
-let repos = [["UnderSea", "An underwater adventure"], ["MazeGen", "Maze generation and pathfinding"], ["BlehSim", "A little virus simulation"], ["ColorPicker", "RGB/HSV to color name converter"], ["Webponents", "A component based html wrapper for P5.js"]];
+let repos = [
+  new Repo(RepoType.GITSITE, "UnderSea", "An underwater adventure"),
+  new Repo(RepoType.GITSITE, "MazeGen", "Maze generation and pathfinding"),
+  new Repo(RepoType.GITSITE, "BlehSim", "Virus simulation"),
+  new Repo(RepoType.GITSITE, "ColorPicker", "RGB/HSV to color name converter"),
+  new Repo(RepoType.GITSITE, "Collisions", "Physics simulation"),
+  new Repo(RepoType.REPO, "Beng-Project", "Tactile electronics simulation platform", "https://github.com/juicetinliu/BEng-Project/blob/main/README.md", "CircuitTable"),
+  new Repo(RepoType.YOUTUBE, "SMASH", "Virtual reality rageroom", "https://youtu.be/kXrILWj7JlU"),
+  new Repo(RepoType.GITSITE, "Webponents", "Component based html wrapper for P5.js"),
+];
 
 function setup() {
   setupHTML();
--- docs/html.js
+++ docs/html.js
@@ -1,26 +1,27 @@
 function setupHTML(){
   main = new Main();
-  let thgroup = new HorzGroup(main)
-  // let titlePanel = new Panel(thgroup, ['out' ,'round']);
-  // let titleGroup = new HorzGroup(titlePanel);
-  // let title = new Text(titleGroup, "My Stuff");   
   noCanvas();
 
   let hgroup = new HorzGroup(main)
   repos.forEach(r => {
+    let repoDetails = r.details();
+    let repoTitle = repoDetails.title;
+    let repoDescription = repoDetails.description;
+    let repoImage = repoDetails.image;
+    let repoLink = repoDetails.link;
     let p = new Panel(hgroup, ['out' ,'round'], 300, 200);
-    let card = new Card(p, 300, 200, "https://juicetinliu.github.io/"+r[0], 'https://raw.githubusercontent.com/juicetinliu/'+r[0]+'/master/header.png', [r[0],r[1]]);
+    let card = new Card(p, 300, 200, repoLink, repoImage, [repoTitle, repoDescription]);
   });
   
   let bothGroup = new HorzGroup(main);
   
-  let button1 = new trackedButton(bothGroup, ['#FFFFFF', '#AACCFF'], 'https://www.linkedin.com/in/justin-l-aa5259130/', 'linkedin.png', 'LinkedIn');
-  let button2 = new trackedButton(bothGroup, ['#FFFFFF', '#AAAAAA'], 'https://github.com/juicetinliu', 'github.png', 'Github');
+  let button1 = new TrackedButton(bothGroup, ['#FFFFFF', '#AACCFF'], 'https://www.linkedin.com/in/justin-l-aa5259130/', 'linkedin.png', 'LinkedIn');
+  let button2 = new TrackedButton(bothGroup, ['#FFFFFF', '#AAAAAA'], 'https://github.com/juicetinliu', 'github.png', 'Github');
 
   main.createHTML();
 }
 
-class trackedButton extends Webponent{
+class TrackedButton extends Webponent{
   constructor(parent, colors, func = null, iconpath = null, hovertxt = 'Button'){
     super('button', parent);
     this.iconpath = iconpath;
@@ -71,18 +72,11 @@ class trackedButton extends Webponent{
 }
 
 class Card extends Webponent{
-  constructor(parent, w, h, func = null, path = null, hovertxt = ['card', '']){
+  constructor(parent, w, h, func = null, path = null, hovertxt = ['', '']){
     super('card', parent);
     this.path = path;
     this.w = w;
     this.h = h;
-    if(func === null){
-      this.purpose = 'x'; //no purpose
-    }else if(typeof func === 'string'){
-      this.purpose = 'l'; //link
-    }else{
-      this.purpose = 'f'; //function
-    }
     this.func = func;
     this.htxt = hovertxt;
   }
@@ -97,21 +91,10 @@ class Card extends Webponent{
     this.HTML.style('border-radius: 5px;')
     this.HTML.style('overflow: hidden');
     this.HTML.style('position: relative');
-    
-
-    // this.HTML.attribute('onclick', "gtag('event', 'click', {'event_category': 'button', 'event_label': '" + this.htxt[0] + "'});");
-    
+            
     let imgInteraction;
-    if(this.purpose === 'l'){
-      imgInteraction = createA(this.func, '', "_blank" );
-    }else{
-      imgInteraction = createButton('');
-    }
+    imgInteraction = createA(this.func, '', "_blank" );
     imgInteraction.class('cardimg');
-
-    if(this.purpose === 'f'){
-      imgInteraction.mousePressed(this.func);
-    }
     imgInteraction.parent(this.HTML);
     imgInteraction.attribute('title', this.htxt[0] + ": " + this.htxt[1]);
     if(this.w > 0){
@@ -120,15 +103,11 @@ class Card extends Webponent{
     if(this.h > 0){
       imgInteraction.style('height: ' + this.h + 'px');
     }
-    if(this.path === null || this.path === ''){
-      imgInteraction.style('padding: 20px');
-    }else{
-      let icon = createImg(this.path, this.htxt[0] + ": " + this.htxt[1]);
-      icon.class('cardimg');
-      icon.style('height: ' + this.h*1.1 + 'px');
-      // icon.style('width: ' + this.w*1.1 + 'px');
-      icon.parent(imgInteraction);
-    }
+    
+    let cardImage = createImg(this.path, this.htxt[0] + ": " + this.htxt[1]);
+    cardImage.class('cardimg');
+    cardImage.style('height: ' + this.h + 'px');
+    cardImage.parent(imgInteraction);
 
     let textdiv = createDiv();
     textdiv.parent(this.HTML);
